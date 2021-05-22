d323bc3d4860efc7c0b7f233657ccc13ef603830
Jason Cox <jasongenecox@gmail.com>
2021-05-21 21:35:06 -0500

    source for fully-functional version (still need to refactor)

--- src/emu.rs
+++ src/emu.rs
@@ -1,5 +1,9 @@
 use std::num::Wrapping;
 use rand::Rng;
+use ggez::*;
+use ggez::event::{KeyCode, KeyMods};
+use ggez::input::keyboard;
+
 
 
 
@@ -40,8 +44,8 @@ pub struct Chip {
     pub keyboard_flag : bool,
     
 
-    pub delay_timer : u8,
-    pub sound_timer : u8,
+    pub delay_timer : f32,
+    pub sound_timer : f32,
 
     pub stack_pointer : usize,
 
@@ -55,7 +59,7 @@ pub struct Chip {
 fn clear_screen(rf : &mut Chip) {
     rf.cls_flag = true;
 
-    for i in 0..2047 {
+    for i in 0..2048 {
         rf.screen_bitmap[i] = 0;
     }
 }
@@ -63,23 +67,9 @@ fn clear_screen(rf : &mut Chip) {
 
 
 
-// TODO this may be broken
-fn jump_with_v0(ins : u16) {
-    //PC = (ins & 0x0FFF) + data_regs[0];
-}
-
-
-fn draw_to_screen(ins : u16) {
-
-    
-
-}
-
 
 fn jump(pc : &mut usize, ins : u16) {
-    //println!("jmp pc before: {}", pc);
     *pc = (ins & 0x0FFF) as usize;
-    //println!("jmp pc after: {}", pc);
 }
 
 
@@ -89,7 +79,6 @@ for the characters 0 thru F
 */
 pub fn memset_sprite_data(mem : &mut [u8 ; 4096]) {
 
-    // unfortunately, there's not really a better way to encode these values.
 
     // "0"
     mem[0] = 0xF0;
@@ -214,16 +203,15 @@ pub fn memset_sprite_data(mem : &mut [u8 ; 4096]) {
 
 
 
-
-
-pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
+pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16, ctx: &Context) {
 
     rf.cls_flag = false;
     rf.draw_flag = false;
+    rf.keyboard_flag = false;
 
     let opcode : u8 = ((ins & 0xF000) >> 12) as u8;
 
-    //println!("pc: {} : {:#06x}, {:#3x}", pc, ins, opcode);
+    println!("pc: {} : {:#06x}", pc, ins);
 
 
     match opcode {
@@ -242,16 +230,16 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
             }
         },
 
-
         0x1 => jump(pc, ins),
 
         0x2 => {
 
             if rf.stack_pointer < 16 {
                 let nnn : u16 = (ins &  0x0FFF);
-                rf.stack[rf.stack_pointer] = nnn;
+                rf.stack[rf.stack_pointer] = *pc as u16;
                 *pc = nnn.into();
                 rf.stack_pointer += 1;
+                return;
             }
         }
 
@@ -284,14 +272,13 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
 
             let equal : bool = rf.data_regs[vx] == rf.data_regs[vy];
             if ((ins & 0xF) == 0) && equal {
-               *pc += *pc + 2; 
+               *pc += 2; 
             }
         }
 
         0x6 => {
             // constant-value update
             // set vx to nn
-            //println!("Constant-value update!");
             let vx : usize = ((ins & 0x0F00) >> 8) as usize;
             let nn : u8 = (ins & 0x00FF) as u8;
 
@@ -301,7 +288,6 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
         0x7 => {
             // constant-value update
             // add nn to vx
-            //println!("Adding value to register");
             let vx : usize = ((ins & 0x0F00) >> 8) as usize;
             let nn : u8 = (ins & 0x00FF) as u8;
 
@@ -339,27 +325,27 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                 }
 
                 0x4 => {
+                    if (rf.data_regs[vy] as u16) + (rf.data_regs[vx] as u16) > 255 {
+                        rf.data_regs[15] = 1;
+                    }
+                    else {rf.data_regs[15] = 0;}
 
                     let x_wrapped = Wrapping(rf.data_regs[vx]);
                     let y_wrapped = Wrapping(rf.data_regs[vy]);
 
                     rf.data_regs[vx] = (x_wrapped + y_wrapped).0;
-                    if (rf.data_regs[vy] as u16) + (rf.data_regs[vx] as u16) > 255 {
-                        rf.data_regs[15] = 1;
-                    }
-                    else {rf.data_regs[15] = 0;}
                 }
 
                 0x5 => {
+                    if rf.data_regs[vy] > rf.data_regs[vx] {
+                        rf.data_regs[15] = 0;
+                    }
+                    else {rf.data_regs[15] = 1;}
 
                     let x_wrapped = Wrapping(rf.data_regs[vx]);
                     let y_wrapped = Wrapping(rf.data_regs[vy]);
 
                     rf.data_regs[vx] = (x_wrapped - y_wrapped).0;
-                    if rf.data_regs[vy] > rf.data_regs[vx] {
-                        rf.data_regs[15] = 0;
-                    }
-                    else {rf.data_regs[15] = 1;}
                     
 
 
@@ -377,15 +363,15 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
 
                 0x7 => {
                     // sets vx to vy - vx. vf set to 0 if borrow, 1 otherwise.
+                    if rf.data_regs[vy] < rf.data_regs[vx] {
+                        rf.data_regs[15] = 0;
+                    }
+                    else {rf.data_regs[15] = 1;}
 
                     let y_wrapped = Wrapping(rf.data_regs[vy]);
                     let x_wrapped = Wrapping(rf.data_regs[vx]);
 
                     rf.data_regs[vx] = (y_wrapped - x_wrapped).0;
-                    if rf.data_regs[vy] < rf.data_regs[vx] {
-                        rf.data_regs[15] = 0;
-                    }
-                    else {rf.data_regs[15] = 1;}
                 }
 
                 // NOTE: ambiguous instruction depending on choice of implementation
@@ -393,7 +379,7 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                     // NOTE: Comment out this line depending on
                     // whether or not your program needs it.
                     //rf.data_regs[vx] = rf.data_regs[vy];
-                    rf.data_regs[15] = 0b10000000 & rf.data_regs[vx];
+                    rf.data_regs[15] = (0b10000000 & rf.data_regs[vx]) >> 7;
                     rf.data_regs[vx] = rf.data_regs[vx] << 1;
                 }
 
@@ -417,7 +403,7 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                 let vy : usize = ((ins & 0x00F0) >> 4) as usize;
 
                 // skips the next instruction if vx != vy
-                if vx != vy {
+                if rf.data_regs[vx] != rf.data_regs[vy] {
                     *pc += 2;
                 }
             }
@@ -464,14 +450,16 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
 
             let x = (rf.data_regs[vx] % 64);
             let y = (rf.data_regs[vy] % 32);
+            
+            rf.data_regs[15] = 0;
 
-            println!("x: {}, y: {}", x,y);
+            //println!("x: {}, y: {}", x,y);
             
             let mut pixels_turned_off : bool = false;
 
             let start_address : usize = rf.address_reg as usize;
 
-            for row in 0..(n) {
+            for row in 0..n {
                 for col in 0..8 {
                     if ((x+col) > 63) || ((y + row) > 31) {
                         break; 
@@ -489,24 +477,18 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                     // TODO this is messy, fix.
 
                     let old_val = rf.screen_bitmap[screen_idx];
-                    rf.screen_bitmap[screen_idx] = rf.screen_bitmap[screen_idx] ^ (bit & rf.memory[offset_address]);
+
+                    let mut sprite_val = 0;
+                    if (bit & rf.memory[offset_address]) > 0 {
+                        sprite_val = 255;
+                    }
+
+                    rf.screen_bitmap[screen_idx] = rf.screen_bitmap[screen_idx] ^ sprite_val;
                     let new_val = rf.screen_bitmap[screen_idx];
 
                     if new_val < old_val {
                         pixels_turned_off = true;
-                        println!("pixels turned off");
                     }
-
-                    /*
-
-                    if(rf.screen_bitmap[screen_idx] == 0) {
-                        rf.screen_bitmap[screen_idx] = (bit & rf.memory[offset_address]);
-                    }
-                    else {
-                        rf.screen_bitmap[screen_idx] = 0; 
-                        pixels_turned_off = true;
-                    }
-                    */
                 }
             }
             
@@ -515,13 +497,26 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                 rf.data_regs[15] = 1;
             }
 
-            println!("Drawing on screen");
+            //println!("Drawing on screen");
     
         },
 
         0xE => {
+            
+            let xop : u8 = (ins & 0x00FF) as u8;
 
-
+            match xop {
+            
+                // skips next instruction if pressed keycode == contents of vx
+                0x9E => {
+                    // nothing here
+                }
+                // skips if opposite of above
+                0xA1 => {
+                    // nothing here
+                }
+                _ => println!("Invalid instruction!"),
+            }
         }
 
         0xF => {
@@ -533,12 +528,16 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                 // sets vx to delay timer value
                 0x07 => {
                     let vx : usize = ((ins & 0x0F00) >> 8) as usize;
-                    rf.data_regs[vx] = rf.delay_timer;
+                    rf.data_regs[vx] = rf.delay_timer.round() as u8;
                 }
 
+                // gets next keypress and stores in vx
                 0x0A => {
+                    /*
                     let vx : usize = ((ins & 0x0F00) >> 8) as usize;
-                    rf.data_regs[vx] = rf.delay_timer;
+                    let keycode = get_keypress(ctx, true);
+                    rf.data_regs[vx] = keycode;
+                    */
                     rf.keyboard_flag = true;
                     return;
                 }
@@ -547,13 +546,13 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
                 // sets delay timer to val in vx
                 0x15 => {
                     let vx : usize = ((ins & 0x0F00) >> 8) as usize;
-                    rf.delay_timer = rf.data_regs[vx];
+                    rf.delay_timer = rf.data_regs[vx] as f32;
                 }
 
                 // sets sound timer to value in vx
                 0x18 => {
                     let vx : usize = ((ins & 0x0F00) >> 8) as usize;
-                    rf.sound_timer = rf.data_regs[vx];
+                    rf.sound_timer = rf.data_regs[vx] as f32;
                 }
 
                 // increments address register by value in vx
@@ -619,17 +618,6 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
 
         }
 
-
-
-
-
-
-
-
-
-
-
-
         _ => println!("ERR: unknown instruction! {:#06x} ", ins)
     }
 
@@ -638,7 +626,7 @@ pub fn decode(rf : &mut Chip, pc : &mut usize, ins: u16) {
 
     // TODO only increment pc if there wasn't a jump
 
-    if !(opcode == 0x1) {*pc = *pc + 2};
+    if !(opcode == 0x1) {*pc += 2};
 
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,88 +2,183 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::time::{Duration, Instant};
 use ggez::*;
 use ggez::event::{KeyCode, KeyMods};
 use ggez::input::keyboard;
+use ggez::audio::SoundSource;
 use ggez::nalgebra as na;
+use std::thread;
 
 
 mod emu;
 
 
-/*
 
-fn wait_for_keypress(rf : &mut emu::RegisterFile) {
+fn play_sound(timer : &mut f32, ctx : &mut Context) {
+    let sound_path = Path::new("/beep.wav");
+    let mut beep = audio::Source::new(ctx, sound_path).unwrap();
+    beep.play();
 
-    loop {
+    //let seconds: u64 = (*timer / 60).round() as u64;
 
-        if keyboard::is_key_pressed(ctx, Key
-
-
-
-
-    }
+    let millis : u64 = (*timer / 0.06).round() as u64;
+    let duration  = Duration::from_millis(millis);
+    thread::sleep(duration);
+    *timer = 0.0;
 }
-*/
 
 
-/*
-fn play_sound(timer : u8, ctx : &mut Context) {
+fn get_keypress(ctx: &mut Context) -> u8 {
 
-    // todo random data, probably change later
-    static sound_bytes : [u8 ; 9] = [255, 255, 255, 255, 255, 255, 255, 255, 255]; 
-    static beep : audio::SoundData = audio::SoundData::from_bytes(&sound_bytes[..]);
-    static source : audio::Source = audio::Source::from_data(ctx, beep);
+    let mut keycode : u8 = 255;
 
-    
-
-    
-    // sound timer should decrement at a rate of 60hz
-    while timer > 0 {
-        source.repeat();
-        //timer -= 60
+    if keyboard::is_key_pressed(ctx, KeyCode::Key1) {
+        keycode = 0x1;
     }
-
+    else if keyboard::is_key_pressed(ctx, KeyCode::Key2) {
+        keycode = 0x2;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::Key3) {
+        keycode = 0x3;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::Key4) {
+        keycode = 0xC;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::Q) {
+        keycode = 0x4;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::W) {
+        keycode = 0x5;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::E) {
+        keycode = 0x6;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::R) {
+        keycode = 0xD;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::A) {
+        keycode = 0x7;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::S) {
+        keycode = 0x8;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::D) {
+        keycode = 0x9;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::F) {
+        keycode = 0xE;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::Z) {
+        keycode = 0xA;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::X) {
+        keycode = 0x0;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::C) {
+        keycode = 0xB;
+    }
+    else if keyboard::is_key_pressed(ctx, KeyCode::V) {
+        keycode = 0xF;
+    }
+    keycode
 }
-*/
+
+
 
 
 struct State {
     dt: std::time::Duration,
     pc : usize,
-    rf : emu::RegisterFile,
+    rf : emu::Chip,
+    current_instruction: u16,
+    delay : u64,
 }
 
 impl ggez::event::EventHandler for State {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         self.dt = timer::delta(ctx);
+        // necessary to control extremely fast fps
+        timer::sleep(Duration::from_millis(self.delay));
 
-        let b1 : u16 = (self.rf.memory[self.pc] as u16 ) << 8;
-        let b2 : u16 = self.rf.memory[self.pc + 1] as u16;
-
-        let next_ins : u16 = b1 | b2;
-
-        emu::decode(&mut self.rf, &mut self.pc, next_ins);
-
-        // TODO probably need a separate thread here for sound
-        /*
-        if rf.sound_timer > 0 {
-            play_sound(rf.sound_timer, ctx);
+    
+        self.rf.delay_timer -= 0.06 * (self.dt.as_millis() as f32);
+        if self.rf.delay_timer <= 0.0 { 
+            self.rf.delay_timer = 0.0;
         }
-        */
 
 
+        
+        if !self.rf.keyboard_flag {
+            let b1 : u16 = (self.rf.memory[self.pc] as u16 ) << 8;
+            let b2 : u16 = self.rf.memory[self.pc + 1] as u16;
 
-
-        /*
-        if rf.keyboard_flag {
-            wait_for_keypress(&mut self.rf);
+            self.current_instruction = b1 | b2;
+            let next_instruction = b1 | b2;
+            emu::decode(&mut self.rf, &mut self.pc, next_instruction, ctx);
         }
-        if keyboard::is_key_pressed(ctx, KeyCode::Key1)
-        */
+
+
+
+        let keycode : u8 = get_keypress(ctx);
+
+        if keycode != 100 {
+            let vx : usize = ((self.current_instruction & 0x0F00) >> 8) as usize;
+            
+            let full_opcode : u16 = (self.current_instruction & 0xF0FF);
+
+            match full_opcode {
+
+                0xE09E => {
+                    if keycode == self.rf.data_regs[vx] {
+                        self.pc += 2;
+                    }
+                }
+
+                0xE0A1 => {
+                    if keycode != self.rf.data_regs[vx] {
+                        self.pc += 2;
+                    }
+                }
+
+                0xF00A => {
+                    self.rf.data_regs[vx] = keycode;
+                }
+                _ => (),
+            }
+            
+            if self.rf.keyboard_flag {
+                self.rf.keyboard_flag = false;
+                self.pc += 2;
+            }
+        }
+        if self.rf.sound_timer > 0.0 {
+            play_sound(&mut self.rf.sound_timer, ctx);
+        }
 
         Ok(())
     }
+
+    fn key_down_event(&mut self, ctx: &mut Context, _keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
+        if _keycode == KeyCode::Escape {
+            event::quit(ctx);
+        }
+    
+        if (_keycode == KeyCode::Tab) && (self.delay < 251) {
+            self.delay += 5;            
+        }
+
+        if (_keycode == KeyCode::LShift) && (self.delay > 4) {
+            self.delay -= 5;            
+        }
+
+    }
+
+
+
+
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         // clears the screen to black
@@ -94,7 +189,7 @@ impl ggez::event::EventHandler for State {
             let mut screen_bitmap_rgba : [u8 ; 8192] = [0 ; 8192];
 
 
-            for i in 0..2047 {
+            for i in 0..2048 {
                 let j = (4*i) as usize;
 
                 let mut set_val = 0;
@@ -102,30 +197,20 @@ impl ggez::event::EventHandler for State {
                 if(self.rf.screen_bitmap[i] != 0) {
                     set_val = 255;
                 }
-                screen_bitmap_rgba[j] = set_val;
+                //screen_bitmap_rgba[j] = set_val;
                 screen_bitmap_rgba[j+1] = set_val;
-                screen_bitmap_rgba[j+2] = set_val;
+                //screen_bitmap_rgba[j+2] = set_val;
                 // alpha channel
                 screen_bitmap_rgba[j+3] = 255;
             }
-
-
-            //let screen_slice = &self.rf.screen_bitmap[..];
             let screen_slice = &screen_bitmap_rgba[..];
-
-
-
             //TODO bottom right corner isn't set for some reason
-
-
             let screen : graphics::Image = ggez::graphics::Image::from_rgba8(ctx, 64, 32, screen_slice).unwrap(); 
             graphics::draw(ctx, &screen, (na::Point2::new(0.0,0.0),))?;
 
             // actually puts stuff on the screen
             graphics::present(ctx)?;
         }
-
-
         Ok(())
     }
 }
@@ -155,7 +240,7 @@ fn main() {
 
     let mut memory : [u8 ; 4096] = [0 ; 4096];
 
-    for i in (0..byte_buffer.len()+1).step_by(2) {
+    for i in (0..byte_buffer.len()-1).step_by(2) {
         memory[512+i] = byte_buffer[i];
         memory[512+i+1] = byte_buffer[i+1];
     }
@@ -163,22 +248,37 @@ fn main() {
 
     emu::memset_sprite_data(&mut memory);
 
-    let mut rf = emu::RegisterFile {
+    let mut rf = emu::Chip {
         data_regs: [0 ; 16],
         address_reg: 0,
         screen_bitmap: [0 ; 2048],
         cls_flag: false,
         draw_flag: false,
         keyboard_flag: false,
-        delay_timer: 0,
-        sound_timer: 0,
+        delay_timer: 0.0,
+        sound_timer: 0.0,
         memory: memory,
         stack: [0 ; 16],
         stack_pointer: 0,
     };
 
 
-    let state = &mut State { dt: std::time::Duration::new(0, 0), pc : 512, rf : rf};
+
+    let state = &mut State { dt: std::time::Duration::new(0, 0), pc : 512, rf : rf, current_instruction : 0, delay: 1};
+
+
+    let window_cfg : conf::WindowMode = conf::WindowMode {
+        width : 960.0,
+        height : 480.0,
+        maximized: false,
+        fullscreen_type: conf::FullscreenType::Windowed,
+        borderless: false,
+        min_width: 0.0,
+        max_width: 0.0,
+        min_height: 0.0,
+        max_height: 0.0,
+        resizable: false,
+    };
 
     let c = conf::Conf::new();
     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("CHIP8-EMU", "jasoncox")
@@ -186,12 +286,13 @@ fn main() {
         .build()
         .unwrap();
 
+    graphics::set_default_filter(ctx, graphics::FilterMode::Nearest);
 
 
     let rect : graphics::Rect = graphics::Rect::new(0.0, 0.0, 64.0, 32.0);
 
+    graphics::set_mode(ctx, window_cfg);
     graphics::set_screen_coordinates(ctx, rect);
-
     graphics::set_window_title(ctx, "CHIP-8 EMU");
 
 
